e79537b4c95e2bf4b74d44af1cfe413d924e3fbd
Hanna-Kisel <hanna.kisiel.pl@gmail.com>
2026-02-04 00:30:35 +0100

    added storage and state

---
+++ src/js/core/storage.js
@@ -0,0 +1,19 @@
+const localStorageKey = 'taskly-tasks'
+
+function getDataFromStorage() {
+    const value = localStorage.getItem(localStorageKey)
+    if (!value) {
+        // localStorage.setItem(localStorageKey, JSON.stringify([]))
+        return []
+    }
+    return JSON.parse(value)
+}
+
+function setDataToStorage(tasks) {
+    localStorage.setItem(localStorageKey, JSON.stringify(tasks))
+}
+
+export {
+    getDataFromStorage,
+    setDataToStorage,
+}
